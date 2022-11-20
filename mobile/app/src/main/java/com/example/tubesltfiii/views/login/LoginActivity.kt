7be6c67439e553f66aa2f7497d9fef51f3623db7
MSMazaya <msulthanmazaya@gmail.com>
2022-11-20 10:49:45 +0700

    feat: add navigation from bluetooth page to main page via recycler view

--- mobile/app/src/main/java/com/example/tubesltfiii/views/login/LoginActivity.kt
+++ mobile/app/src/main/java/com/example/tubesltfiii/views/login/LoginActivity.kt
@@ -3,15 +3,15 @@ package com.example.tubesltfiii.views.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tubesltfiii.databinding.ActivityLoginBinding
 import com.example.tubesltfiii.views.bluetooth.Bluetooth
-import com.example.tubesltfiii.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener {
