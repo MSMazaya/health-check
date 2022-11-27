eaa1704f445db99a426a5fc037e459a47a7c8451
MSMazaya <msulthanmazaya@gmail.com>
2022-11-27 15:45:26 +0700

    remove dotenv for production

--- backend/utils/db.ts
+++ backend/utils/db.ts
@@ -1,7 +1,5 @@
 import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
-import { config } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
 
-config({ export: true });
 const env = Deno.env.toObject();
 
 const client = new MongoClient();
